f291b676880a9ec5edfee26ead55082dcec1f229
prince nyarko <princenyarko360@gmail.com>
2025-09-10 17:40:38 +0000

    Add python_introduction directory with basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 5
+number2 = 10
+sum = number2 + number1
+difference = number2 - number1
+product = number1 * number2
+print(f"Addition of {number2} and {number1} is {sum}")
+print(f"Subtraction of {number2} and {number1} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {product}")   
