0705d4cc40a70846a350b327d0735f15e40ab668
prince nyarko <princenyarko360@gmail.com>
2025-09-25 16:46:33 +0000

    Add arithmetic_operations.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,28 @@
+def perform_operation(num1,num2,operation):
+	num1 : float()
+	num2 : float()
+	operations : str("add,subtract,multiply,divide")
+	match operation:
+		case "add":
+			result = num1 + num2
+
+		case "subtract":
+			result = num1 - num2
+
+		case "multiply":
+			result = num1 * num2
+
+		case "divide":
+			if num2 == 0:
+				print("You cannot divide by zero")
+				result = "Error"
+			else:
+				result = num1 / num2
+		case _:
+			print("Invalid operation")
+			result = "Error"
+
+
+	return result
+	
+
