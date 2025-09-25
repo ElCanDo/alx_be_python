ca4f79cf601ff7672245650542b41f08471400f6
prince nyarko <princenyarko360@gmail.com>
2025-09-25 17:45:57 +0000

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,24 +1,26 @@
-def perform_operation(num1, num2, operation):
+def perform_operation(num1,num2,operation):
+	num1 = float()
+	num2 = float()
+	operation = str("add,subtract,multiply,divide")
 	
-	match operation:
-		case "add":
-			result = num1 + num2
+	if operation == "add":
+		result = num1 + num2
 
-		case "subtract":
-			result = num1 - num2
+	elif operation == "subtract":
+		result = num1 - num2
 
-		case "multiply":
-			result = num1 * num2
+	elif operation == "multiply":
+		result = num1 * num2
 
-		case "divide":
-			if num2 == 0:
-				print("You cannot divide by zero")
-				result = "Error"
-			else:
-				result = num1 / num2
-		case _:
-			print("Invalid operation")
+	elif operation == "divide":
+		if num2 == 0:
+			print("You cannot divide by zero")
 			result = "Error"
+		else:
+			result = num1 / num2
+	else:
+		print("Invalid operation")
+		result = "Error"
 
 
 	return result
