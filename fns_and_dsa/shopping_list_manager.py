dc5b8ecac257ea0415a288851ca4087016caf66d
prince nyarko <princenyarko360@gmail.com>
2025-09-26 15:49:34 +0000

    Add shopping_list_manager.py

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,35 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            item = input("Enter name of item you want to add: ")
+            shopping_list.append(item)# Prompt for and add an item
+            pass
+        elif choice == '2':
+            remove_item = input("Enter name of item you want to remove: ")
+            if remove_item in shopping_list:
+                shopping_list.remove(remove_item)# Prompt for and remove an item
+            else:
+                print("Item not found in list.")
+            pass
+        elif choice == '3':
+            print(shopping_list)# Display the shopping list
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
