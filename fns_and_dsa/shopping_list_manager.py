48f71e2c7895448d8cea86b25765839bc4d00eef
prince nyarko <princenyarko360@gmail.com>
2025-09-26 15:57:45 +0000

    Modify shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -9,21 +9,21 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = input("Enter your choice: ")
+        choice = input("Enter your choice: ") #Promt for picking an item from menu
 
         if choice == '1':
-            item = input("Enter name of item you want to add: ")
-            shopping_list.append(item)# Prompt for and add an item
+            item = input("Enter the item to add: ") 
+            shopping_list.append(item) # Prompt for and add an item
             pass
         elif choice == '2':
             remove_item = input("Enter name of item you want to remove: ")
             if remove_item in shopping_list:
-                shopping_list.remove(remove_item)# Prompt for and remove an item
+                shopping_list.remove(remove_item) # Prompt for and remove an item
             else:
-                print("Item not found in list.")
+                print("Item not found in list.") # Displays if an item to remove is not in list.
             pass
         elif choice == '3':
-            print(shopping_list)# Display the shopping list
+            print(shopping_list) # Display the shopping list
             pass
         elif choice == '4':
             print("Goodbye!")
