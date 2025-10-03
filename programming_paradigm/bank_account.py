8d933319f37bdd0fd896aac3c1c162f582927b2a
prince nyarko <princenyarko360@gmail.com>
2025-10-03 12:50:30 +0000

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,10 +3,17 @@ class BankAccount:
         self.account_balance = account_balance
 
     def deposit(self,amount):
-        self.account_balance += amount
+         if amount <= 0:
+            return False
+         else:
+            self.account_balance += amount
+            return True
 
     def withdraw(self,amount):
-        if amount <= self.account_balance:
+        if amount <= 0:
+            return False
+        
+        elif amount <= self.account_balance:
             self.account_balance -= amount
             return True
         else:
