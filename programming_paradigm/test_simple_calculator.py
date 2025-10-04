7293a1d9833c56313ac7e18aa164e5e59f24584a
prince nyarko <princenyarko360@gmail.com>
2025-10-04 17:04:53 +0000

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -15,19 +15,21 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(0, 0), 0) # check if 0 + 0 = 0
        
 
-    def test_subtract(self):
+    def test_subtraction(self):
         """Test the subtraction method."""
         self.assertEqual(self.calc.subtract(3, 2), 1) # Check if 3 - 2 = 1
         self.assertEqual(self.calc.subtract(-1, 1), -2) # Check if -1 -1 = -2
         self.assertEqual(self.calc.subtract(2, 0), 2) # check if 2 - 0 = 2
         self.assertEqual(self.calc.subtract(1, 1), 0) # check if 1 - 1 = 0
 
-    def test_mutltiply(self):
+    def test_mutltiplication(self):
+        """Test the multiplication method."""
         self.assertEqual(self.calc.multiply(2, 3), 6) # check if 2 * 3 = 6 
         self.assertEqual(self.calc.multiply(-2, 1), -2) # check if -2 * 1 = -2
         self.assertEqual(self.calc.multiply(0, 1), 0) # check if 0 + 1 = 0
     
-    def test_divide(self):
+    def test_division(self):
+        """Test the division method."""
         self.assertAlmostEqual(self.calc.divide(15, 3), 5.0) # check if -1 + 1 = 0
         self.assertAlmostEqual(self.calc.divide(-9, 3), -3.0) # check if -1 + 1 = 0
         self.assertAlmostEqual(self.calc.divide(20, 3), 6.66666667, places=5) # check for floating-point  division
