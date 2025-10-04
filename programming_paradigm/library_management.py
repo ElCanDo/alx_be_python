9227ba1e13a05a99ccc2ff4b4471c372a833aa90
prince nyarko <princenyarko360@gmail.com>
2025-10-04 21:09:29 +0000

    Add library_management.py

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,29 @@
+class Book:
+    def __init__(self, title, author, _is_checked_out = False):
+        self.title = title
+        self.author = author
+        self._is_checked_out = _is_checked_out
+    def check_out_book(self):
+        self._is_checked_out = True
+        # return "Book check out success."
+    def return_book(self):
+        self._is_checked_out = False
+        # return "Book return success."
+    
+
+class Library:
+    def __init__(self,_books = None):
+        if _books is None:
+            self._books = []
+        else:
+            self._books = _books
+    def add_book(self, book):
+        self._books.append(book)
+        
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                book.check_out_book()
+                break
+            else:
+                book._is_checked_out()     
